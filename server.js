55ebb74dd65e52171ce5ec41c5609ee12de19d27
walter <tojimatovabdukarim@gmail.com>
2022-09-03 19:08:25 +0900

    making CV

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 console.log("Web serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
 //1:Kirish kodlari
 app.use(express.static("public"));
 app.use(express.json());
@@ -21,6 +33,10 @@ app.get("/job", function (req, res) {
 });
 app.get("/occupation", function (req, res) {
   res.end("<h1>CEO of Stars of genius</h1>");
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 const server = http.createServer(app);
 let PORT = 3000;
